d35f76cf885e39c605b24970a15cccbda6d7b5f9
Kastubhx <prime.ganekar@gmail.com>
2023-02-22 00:47:30 +0530

    Miscellaneous: Padding & Route New Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/homepage/homepage.component';
+import { ProjectdetailpageComponent } from './pages/projectdetailpage/projectdetailpage.component';
 
 const routes: Routes = [
+  { path: 'project', component: ProjectdetailpageComponent },
   { path: '', component: HomePageComponent },
 ];
 
